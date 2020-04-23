b8ef33e85166d47b9a0bbe83eb148be403b03faf
Emily Ross <e.ross2@setonhill.edu>
2020-04-22 21:43:44 -0400

    Initial Commit

---
+++ 9Tests/_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _Tests.swift
+//  9Tests
+//
+//  Created by Emily Ross on 4/22/20.
+//  Copyright © 2020 Emily Ross. All rights reserved.
+//
+
+import XCTest
+@testable import Ross___Homework_7_9
+
+class _Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
